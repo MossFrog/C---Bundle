495d4925f473199797364709446bcd16fa334657
MossFrog <kaanozcelik@sabanciuniv.edu>
2015-12-21 00:01:15 +0200

    Added the CMD input demonstration.

---
+++ CMD-input.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+//-- A simple demonstration of getting windows CMD input in a windows form --//
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.FileName = "cmd.exe";
+            p.StartInfo.Arguments = "/c ipconfig /all";
+            p.StartInfo.RedirectStandardError = true;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+
+            p.Start();
+
+            StreamReader outputWriter = p.StandardOutput;
+            
+            mainRichTextBox.AppendText(outputWriter.ReadToEnd());
+        }
+    }
+}
