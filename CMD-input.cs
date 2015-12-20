0b17cd5e57980f3ac0c019425c1796221ff6c69f
MossFrog <kaanozcelik@sabanciuniv.edu>
2015-12-21 00:19:08 +0200

    Updated the windows command example.

--- CMD-input.cs
+++ CMD-input.cs
@@ -7,10 +7,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
 using System.IO;
 
 
 //-- A simple demonstration of getting windows CMD input in a windows form --//
+//-- This program has to be run as an administrator to work --//
 namespace Test
 {
     public partial class Form1 : Form
@@ -22,10 +24,12 @@ namespace Test
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.Text = "Windows CMD Test";
+
             System.Diagnostics.Process p = new System.Diagnostics.Process();
             p.StartInfo.UseShellExecute = false;
             p.StartInfo.FileName = "cmd.exe";
-            p.StartInfo.Arguments = "/c ipconfig /all";
+            p.StartInfo.Arguments = "/c ipconfig/all";
             p.StartInfo.RedirectStandardError = true;
             p.StartInfo.RedirectStandardInput = true;
             p.StartInfo.RedirectStandardOutput = true;
@@ -33,8 +37,8 @@ namespace Test
             p.Start();
 
             StreamReader outputWriter = p.StandardOutput;
-            
-            mainRichTextBox.AppendText(outputWriter.ReadToEnd());
+
+            mainRichTextBox.Text += outputWriter.ReadToEnd();
         }
     }
 }
